44990851043e5a8743ddaeacefa6012853aa7ed3
NicKAnderson038 <andersonnick038@gmail.com>
2019-01-13 11:37:47 -0800

    eslinter installed

--- src/components/UI/PickupCard/PickupCardTimes/PickupCardDetails/PickupCardDetails.js
+++ src/components/UI/PickupCard/PickupCardTimes/PickupCardDetails/PickupCardDetails.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import * as classes from './PickupCardDetails.module.css'
+import React from 'react';
+import * as classes from './PickupCardDetails.module.css';
 
 const pickupCardDetails = props => {
-    console.log('DETAILS', props)
+    console.log('DETAILS', props);
     let display = null;
     if(props.changed){
         display = props.showDetail.deliveryTimes.map((delivery,i) => {
             return(
-            <div>
-                <div className={classes.DetailWrapper} key={`delivery-details-${i}`}>
-                    <p>Pickup: {delivery.pickupTime}</p>
-                    <p>Amount: {delivery.amountPerDelivery}</p>
+                <div>
+                    <div className={classes.DetailWrapper} key={`delivery-details-${i}`}>
+                        <p>Pickup: {delivery.pickupTime}</p>
+                        <p>Amount: {delivery.amountPerDelivery}</p>
+                    </div>
+                    <div className={classes.DetailWrapper} key={`delivery-details-${i}`}>     
+                        <p>Delivery: {delivery.deliveryTime}</p>
+                    </div>
+                    <hr></hr>
                 </div>
-                <div className={classes.DetailWrapper} key={`delivery-details-${i}`}>     
-                    <p>Delivery: {delivery.deliveryTime}</p>
-                </div>
-                <hr></hr>
-            </div>
-            )
-        })
+            );
+        });
        
     }else{
         display = null;
     }
 
-  return (
-    <div>
-        <hr></hr>
-      {display}
-    </div>
-  )
-}
+    return (
+        <div>
+            <hr></hr>
+            {display}
+        </div>
+    );
+};
 
-export default pickupCardDetails
-
+export default pickupCardDetails;
